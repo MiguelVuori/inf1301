1f944c0de6b37de2cf81195ec9d4d0bed601270f
Lucas Ximenes <lximenes99@gmail.com>
2020-04-01 18:56:22 -0300

    adciona o tad vetor e termina funções pilha

--- TADPilha/pilha.c
+++ TADPilha/pilha.c
@@ -1,5 +1,7 @@
 #include "pilha.h"
 #include "lista.h"
+#include "stdlib.h"
+#include <stdio.h>
 
 struct pilha {
     Lista* lst;
@@ -9,7 +11,7 @@ Pilha* pilha_cria(void)
 {
     Pilha* new;
     new = malloc(sizeof(Pilha));
-    Lista lista = lst_cria();
+    Lista *lista = lst_cria();
 
     if(new == NULL){
         printf("\n Não foi possível alocar espaço na memória! \n");
@@ -21,7 +23,7 @@ Pilha* pilha_cria(void)
     return new;
 }
 
-int pilha_vazia(Pilha* p);
+int pilha_vazia(Pilha* p)
 {
     if (p == NULL || lst_vazia(p->lst))
         return 1;
@@ -32,20 +34,28 @@ int pilha_vazia(Pilha* p);
 void pilha_push(Pilha* p, void* v)
 {
     if(p != NULL)
-        lst_insIni( p->lis , v );
+        lst_insIni( p->lst , v );
+    else
+    {
+        printf("Nada inserido pois endereço da pilha está errado!");
+    }
     
 }
 
 void *pilha_pop(Pilha* p)
 {
-
+    if (p != NULL || !lst_vazia(p->lst))
+    {
+        lst_retIni(p->lst);
+    }
+    return NULL;    
 }
 
 void pilha_libera(Pilha *p)
 {
     if (p != NULL)
     {
-        lst_libera(p->lis);
+        lst_libera(p->lst);
         free(p);
     }
 
