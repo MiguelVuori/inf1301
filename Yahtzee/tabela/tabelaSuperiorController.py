f6b8a39e0cb03d4a17ddaa3f4230a8c906b288ea
mfvuori <mfvuori@gmail.com>
2020-06-01 16:52:23 -0300

    modificando pequenas coisas

--- Yahtzee/tabela/tabelaSuperiorController.py
+++ Yahtzee/tabela/tabelaSuperiorController.py
@@ -1,11 +1,14 @@
 from tabelaModel import Tabela
 from tabelaSuperiorView import ViewSuperior
+from tkinter import messagebox
 
 
 class TabelaSuperior():
-    def __init__(self, root):
-        self.tabela = Tabela('Lucas')
+    def __init__(self, root , nome):
+        self.tabela = Tabela(nome)
         self.view = ViewSuperior(root, self.tabela)
         self.view.mostra()
+
+        messagebox.showinfo("Inclusão Efetuada")
 
 
